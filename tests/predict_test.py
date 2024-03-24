4e6a01c699d109a7e2791f8874c80c1074bfb0bf
Sofosss <argsofos.1@gmail.com>
2024-03-24 22:53:39 +0200

    Remove unusable imports | Added comments
    Co-authored-by: DmMeta <dimetaxakis@gmail.com>

--- tests/predict_test.py
+++ tests/predict_test.py
@@ -1,46 +1,43 @@
 import requests
 import pandas as pd
 import json
-from pprint import pprint as pp
 from sklearn.metrics import classification_report
-import joblib
-import numpy as np
 from sklearn.utils import shuffle
 import random as rnd
-#predict endpoint
+
+# Predict endpoint
 PREDICT_ENDPOINT = 'http://127.0.0.1:8891/predict'
-SLICE = 15000
-
-
+SLICE = 15000  # Number of samples to use
 
 def main():
+    # Read test data
     X_test = pd.read_csv('../data/test_sample.csv')
     y_test = pd.read_csv('../data/test_sample_labels.csv')
     
+    # Shuffle test data
     X_test_shuffled, y_test_shuffled = shuffle(X_test, y_test, random_state=rnd.randint(0,len(y_test)))
     
+    # Prepare request data
     flow_request = {
-        "model": "ANN",
-        "query": X_test_shuffled.iloc[:SLICE].to_dict(orient = 'records')
+        "model": "CNN_LSTM",  # Specify the model
+        "query": X_test_shuffled.iloc[:SLICE].to_dict(orient='records')  # Convert test data to dictionary
     }
 
-    # pp(json.dumps(flow_request))
-    
+    # Send POST request to the predict endpoint
     response = requests.post(PREDICT_ENDPOINT, json=json.dumps(flow_request),
-                             headers = {"Content-Type": "application/json"})  # Use json parameter instead of data
+                             headers={"Content-Type": "application/json"})  
     
+    # Check response status
     if response.status_code == 200:
+        # If successful, parse response and generate classification report
         response_dict = json.loads(response.text)
         target_categories = set(y_test_shuffled.values[:SLICE].flatten())
         class_report = classification_report(response_dict["Predictions"], y_test_shuffled.iloc[:SLICE], target_names=target_categories, digits=6)
         print("Classification Report:\n", class_report)
-
     else:
-        print(f"Error occured!")
+        # If error occurs, print error message
+        print(f"Error occurred!")
         print(response.text)
    
-    
-
-
 if __name__ == '__main__':
-    main()
+    main()  # Call the main function
