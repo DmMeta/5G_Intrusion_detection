fc76a4672299d8e82ad3379fd79dcfad46b2a29b
Sofosss <argsofos.1@gmail.com>
2024-03-24 23:16:31 +0200

    Added comments & Remove Unusable Imports
    Co-authored-by: DmMeta <dimetaxakis@gmail.com>

--- src/model.py
+++ src/model.py
@@ -5,22 +5,51 @@ import torch
 import pandas as pd
 
 class ModelAdapter(ABC):
+    """
+    Abstract base class for the different model adapters.
+    """
 
     def __init__(self, config_path="./config.yml"):
+        """
+        Initialize the ModelAdapter.
+
+        Args:
+            config_path (str, optional): The path to the configuration file. Defaults to "./config.yml".
+        """
         self._config = self._load_config(config_path)
         self.features = self._load_features(self._config['features_path'])
 
     @abstractmethod
-    def _load_model(self,):
+    def _load_model(self):
+        """
+        Abstract method to load the model.
+        """
         pass
+
     @abstractmethod
-    def _preprocess(self,):
+    def _preprocess(self, data):
+        """
+        Abstract method to preprocess the data.
+        """
         pass
+
     @abstractmethod
-    def predict(self,):
+    def predict(self, data):
+        """
+        Abstract method to make predictions.
+        """
         pass
     
     def _load_config(self, config_path):
+        """
+        Load the models' configuration from a YAML file.
+
+        Args:
+            config_path (str): The path to the configuration file.
+
+        Returns:
+            dict: The loaded configuration.
+        """
         with open(config_path, 'r') as stream:
             try:
                 return yaml.safe_load(stream)
@@ -29,13 +58,33 @@ class ModelAdapter(ABC):
                 return None
 
     def _load_features(self, features_path):
+        """
+        Load the features from a file.
+
+        Args:
+            features_path (str): The path to the features file.
+
+        Returns:
+            object: The loaded features.
+        """
         with open(features_path, 'rb') as file:
             features = joblib.load(file)
         return features
 
-
     @classmethod
-    def select_model(cls, model_name = "RandomForest"):
+    def select_model(cls, model_name="RandomForest"):
+        """
+        Select the appropriate model based on the model name.
+
+        Args:
+            model_name (str, optional): The name of the model to select. Defaults to "RandomForest".
+
+        Returns:
+            ModelAdapter: An instance of the selected model adapter.
+        
+        Raises:
+            ValueError: If the model name is not supported.
+        """
         if model_name == 'ANN':
             return ANNAdapter()
         elif model_name == "CNN":
@@ -46,31 +95,50 @@ class ModelAdapter(ABC):
             return ScikitTreeBasedModel(model_name)
         else:
             raise ValueError("Invalid model type. Not supported")
-# Slight modification to the way the model is selected in server
-# now through ModelAdapter.select_model(model_name)
-
 
 class ANNAdapter(ModelAdapter):
+    """
+    Model adapter for Artificial Neural Network (ANN) models.
+    """
+
     def __init__(self):
+        """
+        Initialize the ANN adapter.
+        """
         super(ANNAdapter, self).__init__()
         self.model = self._load_model()
 
-    def _load_model(self,):
+    def _load_model(self):
+        """
+        Load the ANN model.
+
+        Returns:
+            object: The loaded ANN model.
+        """
         from model_definition import ANN_BinaryClassifier
 
         input_size = self._config['NN']['ANN']['input_size']
         hidden_size = self._config['NN']['ANN']['hidden_size']
         output_size = self._config['NN']['ANN']['output_size']
 
-        model = ANN_BinaryClassifier(input_size = input_size, 
-                                     hidden_size = hidden_size, 
-                                     output_size = output_size)
+        model = ANN_BinaryClassifier(input_size=input_size, 
+                                     hidden_size=hidden_size, 
+                                     output_size=output_size)
         with open(self._config['NN']['ANN']['model_path'], 'rb') as file:
             model.load_state_dict(joblib.load(file))
 
         return model
 
     def _preprocess(self, data):
+        """
+        Preprocess the data for the ANN model.
+
+        Args:
+            data (list): The input data to preprocess.
+
+        Returns:
+            torch.FloatTensor: The preprocessed data.
+        """
         dfs = []
 
         for query_flow in data:
@@ -90,6 +158,15 @@ class ANNAdapter(ModelAdapter):
         return torch.FloatTensor(y_out)
 
     def predict(self, data):
+        """
+        Make predictions using the ANN model.
+
+        Args:
+            data (list): The input data for prediction.
+
+        Returns:
+            dict: A dictionary containing the predictions.
+        """
         y = self._preprocess(data)
         y_pred = (self.model(y) > 0.5).int()
 
@@ -100,13 +177,24 @@ class ANNAdapter(ModelAdapter):
         y_pred_labels = t_encoder.inverse_transform(y_pred.ravel())
         return {"Predictions": y_pred_labels.tolist()}
 
-
 class CNNAdapter(ModelAdapter):
+    """
+    Model adapter for Convolutional Neural Network (CNN) models.
+    """
     def __init__(self):
+        """
+        Initializes a CNNAdapter instance.
+        """
         super(CNNAdapter, self).__init__()
         self.model = self._load_model()
 
     def _load_model(self,):
+        """
+        Loads the CNN model from the specified path.
+
+        Returns:
+            object: The loaded CNN model.
+        """
         from model_definition import CNN_BinaryClassifier
 
         classes = self._config['NN']['CNN']['classes']
@@ -124,6 +212,15 @@ class CNNAdapter(ModelAdapter):
         return model
 
     def _preprocess(self, data):
+        """
+        Preprocesses the input data for the CNN model.
+
+        Args:
+            data (list): The input data to preprocess.
+
+        Returns:
+            torch.FloatTensor: The preprocessed data.
+        """
         dfs = []
 
         for query_flow in data:
@@ -144,6 +241,15 @@ class CNNAdapter(ModelAdapter):
         return torch.FloatTensor(y_out)
 
     def predict(self, data):
+        """
+        Makes predictions using the CNN model.
+
+        Args:
+            data (list): The input data for prediction.
+
+        Returns:
+            dict: A dictionary containing the predictions.
+        """
         y = self._preprocess(data)
 
         y_pred = (self.model(y) > 0.5).int()
@@ -155,14 +261,21 @@ class CNNAdapter(ModelAdapter):
         y_pred_labels = t_encoder.inverse_transform(y_pred.ravel())
         return {"Predictions": y_pred_labels.tolist()}
 
-
-
 class CNN_LSTMAdapter(ModelAdapter):
     def __init__(self):
+        """
+        Initializes a CNN_LSTMAdapter instance.
+        """
         super(CNN_LSTMAdapter, self).__init__()
         self.model = self._load_model()
 
     def _load_model(self,):
+        """
+        Loads the CNN-LSTM model from the specified path.
+
+        Returns:
+            object: The loaded CNN-LSTM model.
+        """
         from model_definition import CNN_LSTM_BinaryClassifier
 
         classes = self._config['NN']['CNN_LSTM']['classes']
@@ -184,6 +297,15 @@ class CNN_LSTMAdapter(ModelAdapter):
         return model
 
     def _preprocess(self, data):
+        """
+        Preprocesses the input data.
+
+        Args:
+            data (list): The input data to preprocess.
+
+        Returns:
+            torch.FloatTensor: The preprocessed data.
+        """
         dfs = []
 
         for query_flow in data:
@@ -204,6 +326,15 @@ class CNN_LSTMAdapter(ModelAdapter):
         return torch.FloatTensor(y_out)
 
     def predict(self, data):
+        """
+        Makes predictions on the input data.
+
+        Args:
+            data (list): The input data for prediction.
+
+        Returns:
+            dict: A dictionary containing the predictions.
+        """
         y = self._preprocess(data)
         y_pred = (self.model(y) > 0.5).int()
 
@@ -217,17 +348,42 @@ class CNN_LSTMAdapter(ModelAdapter):
 
 class ScikitTreeBasedModel(ModelAdapter):
     def __init__(self, model_name):
+        """
+        Initializes a ScikitTreeBasedModel instance.
+
+        Args:
+            model_name (str): The name of the model.
+
+        Attributes:
+            model_type (str): The type of the model.
+            model: The loaded Scikit-learn model.
+        """
         super(ScikitTreeBasedModel, self).__init__()
         self.model_type = model_name
         self.model = self._load_model()
 
     def _load_model(self,):
+        """
+        Loads the Scikit-learn model from the specified path.
+
+        Returns:
+            object: The loaded Scikit-learn model.
+        """
         model_path = self._config['TREE_BASED_MODELS'][self.model_type]['model_path']
         with open(model_path, 'rb') as file:
             model = joblib.load(file)
         return model
 
     def _preprocess(self, data):
+        """
+        Preprocesses the input data.
+
+        Args:
+            data (list): The input data to preprocess.
+
+        Returns:
+            pandas.DataFrame: The preprocessed data.
+        """
         dfs = []
 
         for query_flow in data:
@@ -241,8 +397,18 @@ class ScikitTreeBasedModel(ModelAdapter):
         return y_out
     
     def predict(self, data):
+        """
+        Makes predictions on the input data.
+
+        Args:
+            data (list): The input data for prediction.
+
+        Returns:
+            dict: A dictionary containing the predictions.
+        """
         y = self._preprocess(data)
 
         y_pred = self.model.predict(y)
        
         return {"Predictions": y_pred.tolist()}
+
--- src/server.py
+++ src/server.py
@@ -1,23 +1,9 @@
-from fastapi import (FastAPI,
-                     HTTPException,
-                     Request,
-                     Response,
-                     status) 
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
-from typing import Dict, List, Union, Any
 import json
-from model import ModelAdapter
-  
-# with open(os.path.join("../models","top_10_features_cols.pkl"), "rb") as ffile:
-#     features = pickle.load(ffile)
+from model import ModelAdapter  # Importing ModelAdapter from the model module
 
-# field_definitions = {field: Union[float,str] for field in features}
-
-# Query = create_model("Query", **field_definitions)
-
-# print(Query.__annotations__.keys())
-
-app = FastAPI() 
+app = FastAPI()  # Creating a FastAPI application instance
 
 class PredictionQuery(BaseModel):
     model: str
@@ -26,24 +12,29 @@ class PredictionQuery(BaseModel):
     class Config:
         arbitrary_types_allowed = True
 
-
-@app.post("/predict")
+@app.post("/predict")  # Endpoint for making predictions
 async def predict(pred_request: Request):
-    
+    # Get JSON payload from the request
     payload = await pred_request.json()
     pred_request = json.loads(payload)
+
+    # Extract model name and query data from the payload
     model_name = pred_request['model']
     y = pred_request['query']
-    print(f"Model: {model_name} Query: {len(y)}")
+    print(f"Model: {model_name} | Number of Samples: {len(y)}")
     
     try:
+        # Select the appropriate model based on the model name
         model = ModelAdapter.select_model(model_name)
+        # Make predictions using the selected model
         prediction = model.predict(y)
     except ValueError as e:
+        # Handle errors if the model is not found
         raise HTTPException(status_code=404, detail=str(e))
 
+    # Return the prediction
     return prediction
 
-@app.get("/health")
+@app.get("/health")  # Endpoint for checking the health of the application
 def health():
-    return {"status": "ok"}
+    return {"status": "ok"}  # Return a JSON response indicating the health status
